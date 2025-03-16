6ecaa9bc59d68fa18315d32908a8f53fd06fb659
Rey Longno <113006340+iCryWhenICodeAt3AM@users.noreply.github.com>
2025-03-16 18:48:36 +0800

    Create config.js

---
+++ static/assets/js/config.js
@@ -0,0 +1,6 @@
+const PUBNUB_SUBSCRIBE_KEY = 'sub-c-e9a921ee-131b-4e6c-a831-cab68463ee42';
+const PUBNUB_PUBLISH_KEY = 'pub-c-e4e0cbb0-e5cb-4969-a27d-b4aeb353af98';
+const API_BASE_URL = 'https://dozmuvdice.execute-api.us-east-2.amazonaws.com/';
+const WEB_CHAT_TOKEN = 'eyJraWQiOiJjdXN0b20tb2F1dGgta2V5aWQiLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+const WEB_HOST_URL = 'learnhub-819075017202269389.myfreshworks.com';
+// Add any other configuration settings here
